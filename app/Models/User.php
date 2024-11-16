c054afd25850c1f619814be3612d57b9adb7fb6a
fjmarquezruiz <fjmarquezruiz@gmail.com>
2024-11-16 17:49:59 +0100

    Setup dark mode to Admin and mail verification

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,16 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+/**
+ * Summary of User
+ * Implements MustVerifyEmail to send an email to confirm
+ */
+class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
